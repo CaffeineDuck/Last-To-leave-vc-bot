41675a2810ad1900a077a2a706849c170a82c010
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-04 12:57:46 +0545

    Now the bots sends message in general about afk check rather than DMs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+import re
 from typing import List
 
 import discord
@@ -30,22 +31,31 @@ class Bot(commands.Bot):
         self.event_bot_kicked_users = []
         self.event_managers = []
 
+    def is_manager(self):
+        def predicate(ctx):
+            return ctx.message.author.id in self.event_managers
+
+        return commands.check(predicate)
+
+    def has_manage_guild_permissions(self):
+        def predicate(ctx: commands.Context):
+            author_perms: discord.Permissions = ctx.channel.permissions_for(ctx.author)
+            return ctx.guild is not None and author_perms.manage_guild
+
+        return commands.check(predicate)
+
     async def dm_random_check(self, member: discord.Member) -> None:
         logging_channel = member.guild.get_channel(EVENT_LOGGING_CHANNEL_ID)
+        general_channel = member.guild.get_channel(GENERAL_ID)
 
         word = random.choice(self.random_words)
 
         def msg_check(m):
             return m.content.lower() == word and m.author.id == member.id
 
-        try:
-            await member.send(
-                f"Reply with `{word}` within the next 120 seconds or get kicked!"
-            )
-        except discord.Forbidden:
-            await logging_channel.send(
-                f"{member.mention} **Send message** `{word}` within the next 120 secs in this server or get kicked!"
-            )
+        await general_channel.send(
+            f"{member.mention} Reply with `{word}` within the next 120 seconds or get disconnected!"
+        )
 
         try:
             await self.wait_for("message", check=msg_check, timeout=120)
@@ -63,10 +73,6 @@ class Bot(commands.Bot):
                 reason="Member didn't respond in time for the LTLVC DM check!",
             )
             await logging_channel.send(embed=embed)
-        except discord.Forbidden:
-            await logging_channel.send(
-                f"{member.mention} passed the AFK check! This message is being shown here as I can't send you DMs"
-            )
 
     async def on_voice_state_update(
         self,
@@ -115,6 +121,27 @@ class Bot(commands.Bot):
     async def on_ready(self):
         print(f"Logged in with {self.user.name}")
 
+    async def on_command_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        if isinstance(error, commands.BotMissingPermissions):
+            return await ctx.reply(
+                f"I am missing the following permissions:\n **{','.join(error.missing_perms)}**"
+            )
+        elif isinstance(error, commands.MissingPermissions):
+            return await ctx.reply(
+                f"You are missing the following permissions:\n**{','.join(error.missing_perms)}**"
+            )
+        else:
+            title = " ".join(
+                re.compile(r"[A-Z][a-z]*").findall(error.__class__.__name__)
+            )
+            return await ctx.send(
+                embed=discord.Embed(
+                    title=title, description=str(error), color=discord.Color.red()
+                )
+            )
+
     @tasks.loop(minutes=random.randrange(15, 25))
     async def message_check(self):
         if not self.event_started:
@@ -136,7 +163,9 @@ class Bot(commands.Bot):
 bot = Bot(command_prefix=BOT_PREFIX)
 
 
-@commands.has_permissions(manage_guild=True)
+@commands.check_any(
+    bot.is_owner(), bot.is_manager(), bot.has_manage_guild_permissions()
+)
 @bot.command()
 async def addmanagers(ctx: commands.Context, members: commands.Greedy[discord.Member]):
     members = [member.id for member in members]
@@ -144,7 +173,9 @@ async def addmanagers(ctx: commands.Context, members: commands.Greedy[discord.Me
     await ctx.send("Added them as event managers!")
 
 
-@commands.has_permissions(manage_guild=True)
+@commands.check_any(
+    bot.is_owner(), bot.is_manager(), bot.has_manage_guild_permissions()
+)
 @bot.command()
 async def managers(ctx: commands.Context):
     try:
@@ -157,35 +188,50 @@ async def managers(ctx: commands.Context):
         await ctx.send("There are no managers yet!")
 
 
-@commands.has_permissions(manage_guild=True)
+@commands.check_any(
+    bot.is_owner(), bot.is_manager(), bot.has_manage_guild_permissions()
+)
 @bot.command()
 async def addwords(ctx: commands.Context, *words):
     bot.random_words.extend(words)
     await ctx.send("New random words added to this list!")
 
 
-@commands.has_permissions(manage_guild=True)
+@commands.check_any(
+    bot.is_owner(), bot.is_manager(), bot.has_manage_guild_permissions()
+)
 @bot.command()
 async def words(ctx: commands.Context):
     await ctx.send(", ".join(bot.random_words))
 
 
-@commands.has_permissions(manage_guild=True)
+@commands.check_any(
+    bot.is_owner(), bot.is_manager(), bot.has_manage_guild_permissions()
+)
 @bot.command()
 async def start(ctx: commands.Context):
     bot.event_started = True
-    bot.message_check.start()
-    ltlvc_channel = bot.get_channel(EVENT_LOGGING_CHANNEL_ID)
 
-    await ltlvc_channel.send(
-        embed=discord.Embed(
-            description="**Last To Leave VC Event** has started!", title="EVENT STARTED"
-        )
+    try:
+        bot.message_check.start()
+    except RuntimeError:
+        bot.message_check.restart()
+
+    ltlvc_channel = bot.get_channel(EVENT_LOGGING_CHANNEL_ID)
+    general_channel = bot.get_channel(GENERAL_ID)
+
+    embed = discord.Embed(
+        description="**Last To Leave VC Event** has started!", title="EVENT STARTED"
     )
+
+    await general_channel.send(embed=embed)
+    await ltlvc_channel.send(embed=embed)
     await ctx.send("LTLVC event has been started!")
 
 
-@commands.has_permissions(manage_guild=True)
+@commands.check_any(
+    bot.is_owner(), bot.is_manager(), bot.has_manage_guild_permissions()
+)
 @bot.command()
 async def stop(ctx: commands.Context):
     bot.event_started = False
